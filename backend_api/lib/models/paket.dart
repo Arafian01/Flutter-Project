5909f6dc41d9fbf729a4cfc132568596036bbfac
Arafian Hafiz <arafianw@gmail.com>
2025-04-25 23:19:25 +0700

    Update paket.dart

--- backend_api/lib/models/paket.dart
+++ backend_api/lib/models/paket.dart
@@ -1,31 +1,29 @@
 class Paket {
-  final int id;
+  final int? id;
   final String namaPaket;
   final String deskripsi;
   final int harga;
 
-  Paket({
-    required this.id,
-    required this.namaPaket,
-    required this.deskripsi,
-    required this.harga,
-  });
+  Paket({ this.id, required this.namaPaket, required this.deskripsi, required this.harga });
 
-  // Fungsi untuk membuat instance Paket dari row hasil query
-  factory Paket.fromRow(List<dynamic> row) {
-    return Paket(
-      id: row[0] as int,
-      namaPaket: row[1] as String,
-      deskripsi: row[2] as String,
-      harga: row[3] as int,
-    );
-  }
+  factory Paket.fromRow(List<dynamic> row) => Paket(
+    id: row[0] as int,
+    namaPaket: row[1] as String,
+    deskripsi: row[2] as String,
+    harga: row[3] as int,
+  );
 
-  // Fungsi untuk mengubah data Paket menjadi format JSON
+  factory Paket.fromJson(Map<String, dynamic> json) => Paket(
+    id: json['id'] as int?,
+    namaPaket: json['nama_paket'] as String,
+    deskripsi: json['deskripsi'] as String,
+    harga: json['harga'] as int,
+  );
+
   Map<String, dynamic> toJson() => {
-        'id': id,
-        'nama_paket': namaPaket,
-        'deskripsi': deskripsi,
-        'harga': harga,
-      };
+    if (id != null) 'id': id,
+    'nama_paket': namaPaket,
+    'deskripsi': deskripsi,
+    'harga': harga,
+  };
 }
