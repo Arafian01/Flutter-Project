31335f75138e809f7cecf7ee98728c2453d4921b
Arafian Hafiz <arafianw@gmail.com>
2025-04-25 09:35:34 +0700

    Update top_navbar.dart

--- project_wifi/lib/widgets/top_navbar.dart
+++ project_wifi/lib/widgets/top_navbar.dart
@@ -2,16 +2,23 @@ import 'package:flutter/material.dart';
 
 class TopNavbar extends StatelessWidget implements PreferredSizeWidget {
   @override
-  Size get preferredSize => Size.fromHeight(60);
+  Size get preferredSize => const Size.fromHeight(60);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
+      automaticallyImplyLeading: false, // Menghilangkan tombol back
       backgroundColor: Colors.red,
-      title: Text('StrongNet'),
+      title: const Text(
+        'StrongNet',
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
       actions: [
         IconButton(
-          icon: Icon(Icons.person),
+          icon: const Icon(Icons.person, color: Colors.white),
           onPressed: () {
             // Aksi ketika ikon diklik
           },
