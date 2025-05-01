1bd4c07bc75c9019324ce44953212a195c30fd12
Arafian Hafiz <arafianw@gmail.com>
2025-05-01 20:00:38 +0700

    Update paket.dart

--- project_wifi/lib/models/paket.dart
+++ project_wifi/lib/models/paket.dart
@@ -1,29 +1,34 @@
+// lib/models/paket.dart
+
 class Paket {
-  final int? id;
+  final int id;
   final String namaPaket;
   final String deskripsi;
-  final int harga;
+  final double harga;
 
-  Paket({ this.id, required this.namaPaket, required this.deskripsi, required this.harga });
+  Paket({
+    required this.id,
+    required this.namaPaket,
+    required this.deskripsi,
+    required this.harga,
+  });
 
-  factory Paket.fromRow(List<dynamic> row) => Paket(
-    id: row[0] as int,
-    namaPaket: row[1] as String,
-    deskripsi: row[2] as String,
-    harga: row[3] as int,
-  );
+  /// Buat instance Paket dari JSON
+  factory Paket.fromJson(Map<String, dynamic> json) {
+    return Paket(
+      id: json['id'] as int,
+      namaPaket: json['nama_paket'] as String,
+      deskripsi: json['deskripsi'] as String,
+      harga: (json['harga'] as num).toDouble(),
+    );
+  }
 
-  factory Paket.fromJson(Map<String, dynamic> json) => Paket(
-    id: json['id'] as int?,
-    namaPaket: json['nama_paket'] as String,
-    deskripsi: json['deskripsi'] as String,
-    harga: json['harga'] as int,
-  );
-
-  Map<String, dynamic> toJson() => {
-    if (id != null) 'id': id,
-    'nama_paket': namaPaket,
-    'deskripsi': deskripsi,
-    'harga': harga,
-  };
+  /// Konversi Paket ke JSON untuk request
+  Map<String, dynamic> toJson() {
+    return {
+      'nama_paket': namaPaket,
+      'deskripsi': deskripsi,
+      'harga': harga,
+    };
+  }
 }
