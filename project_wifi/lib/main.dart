0a0a1e283c6c7825444104e0d71bc9af43c930c6
Arafian Hafiz <arafianw@gmail.com>
2025-05-02 13:46:59 +0700

    Update main.dart

--- project_wifi/lib/main.dart
+++ project_wifi/lib/main.dart
@@ -1,9 +1,21 @@
 // lib/main.dart
 import 'package:flutter/material.dart';
+import 'package:project_wifi/models/paket.dart';
+import 'package:project_wifi/models/pelanggan.dart';
+import 'package:project_wifi/models/pembayaran.dart';
+import 'package:project_wifi/models/tagihan.dart';
 import 'widgets/main_layout.dart';
 import 'pages/splash_page.dart';
 import 'pages/login_page.dart';
 import 'pages/register_page.dart';
+import 'pages/admin/paket/add_paket_page.dart';
+import 'pages/admin/paket/edit_paket_page.dart';
+import 'pages/admin/pelanggan/add_pelanggan_page.dart';
+import 'pages/admin/pelanggan/edit_pelanggan_page.dart';
+import 'pages/admin/tagihan/add_tagihan_page.dart';
+import 'pages/admin/tagihan/edit_tagihan_page.dart';
+import 'pages/add_pembayaran_page.dart';
+import 'pages/edit_pembayaran_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,6 +39,26 @@ class MyApp extends StatelessWidget {
         '/splash': (context) => SplashPage(),
         '/login': (context) => const LoginPage(),
         '/register': (context) => RegisterPage(),
+        '/add_paket': (context) => const AddPaketPage(),
+        '/edit_paket': (context) {
+          final paket = ModalRoute.of(context)!.settings.arguments as Paket;
+          return EditPaketPage(paket: paket);
+        },
+        '/add_pelanggan' : (context) => const AddPelangganPage(),
+        '/edit_pelanggan' : (context) {
+          final pelanggan = ModalRoute.of(context)!.settings.arguments as Pelanggan;
+          return EditPelangganPage(pelanggan: pelanggan);
+        },
+        '/add_tagihan' : (context) => const AddTagihanPage(),
+        '/edit_tagihan' : (context) {
+          final tagihan = ModalRoute.of(context)!.settings.arguments as Tagihan;
+          return EditTagihanPage(tagihan: tagihan);
+        },
+        '/add_pembayaran': (_) => const AddPembayaranPage(),
+        '/edit_pembayaran': (ctx) {
+          final pembayaran = ModalRoute.of(ctx)!.settings.arguments as Pembayaran;
+          return EditPembayaranPage(pembayaran: pembayaran);
+        },
       },
       onGenerateRoute: (settings) {
         if (settings.name == '/main') {
