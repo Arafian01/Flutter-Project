57110ef73357acadba57b0aea1259ec2f7e70438
Arafian Hafiz <arafianw@gmail.com>
2025-05-03 10:55:15 +0700

    Delete pembayaran_page.dart

--- project_wifi/lib/pages/pembayaran_page.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class PembayaranPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Center(child: Text('Halaman Pembayaran'));
-  }
-}
