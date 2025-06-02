3a3da6387ee0068f34d14441130b1d9f25b86354
Arafian Hafiz <arafianw@gmail.com>
2025-06-02 20:21:03 +0700

    Update tagihan.dart

--- project_wifi/lib/models/tagihan.dart
+++ project_wifi/lib/models/tagihan.dart
@@ -3,7 +3,8 @@ class Tagihan {
   final int id;
   final int pelangganId;
   final String pelangganName;
-  final String bulanTahun;
+  final int bulan; // Ganti dari bulanTahun
+  final int tahun;
   final String statusPembayaran;
   final DateTime jatuhTempo;
   final int harga;
@@ -12,7 +13,8 @@ class Tagihan {
     required this.id,
     required this.pelangganId,
     required this.pelangganName,
-    required this.bulanTahun,
+    required this.bulan,
+    required this.tahun,
     required this.statusPembayaran,
     required this.jatuhTempo,
     required this.harga,
@@ -22,7 +24,8 @@ class Tagihan {
     id: json['id'] as int,
     pelangganId: json['pelanggan_id'] as int,
     pelangganName: json['pelanggan_name'] as String? ?? '',
-    bulanTahun: json['bulan_tahun'] as String? ?? '',
+    bulan: json['bulan'] as int,
+    tahun: json['tahun'] as int,
     statusPembayaran: json['status_pembayaran'] as String? ?? '',
     jatuhTempo: DateTime.parse(json['jatuh_tempo'] as String),
     harga: (json['harga'] as int?) ?? 0,      // ← gunakan int? dan default 0
@@ -30,8 +33,11 @@ class Tagihan {
 
 
   Map<String, dynamic> toJson() => {
+    'id': id,
     'pelanggan_id': pelangganId,
-    'bulan_tahun': bulanTahun,
+    'bulan': bulan,
+    'tahun': tahun,
+    'harga': harga,
     'status_pembayaran': statusPembayaran,
   };
 }
